ee93c227e6f0d9f426a220cfbaa16e96560fb5b5
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-05-26 00:22:11 +0200

    ajout not found + retour vers l'accueil

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,10 +1,15 @@
 import './Button.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
 
 const Button = () => {
+    const navigate = useNavigate();
+    const backHome = () => {
+        navigate('/');
+    }
     return ( 
-        <div className="buttonBack">
+        <div className="buttonBack" onClick={backHome}>
             <FontAwesomeIcon icon={faArrowLeft} className="logo-arrow"></FontAwesomeIcon>
         </div>
      );
